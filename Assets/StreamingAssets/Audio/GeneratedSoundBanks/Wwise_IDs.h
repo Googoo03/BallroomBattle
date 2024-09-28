4880025c448329955fc6ddc67f1fbecaa0eeaecc
MonstoBusta <119002070+MonstoBusta@users.noreply.github.com>
2024-09-28 14:50:51 -0500

    Basic Wwise events

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID P1_ATTACK = 129085443U;
+        static const AkUniqueID P1_CHARGE = 2336901343U;
+        static const AkUniqueID P1_DEFEND = 3616859791U;
+        static const AkUniqueID P1_MISS = 2205895999U;
+        static const AkUniqueID P1_SPECIAL = 1566201372U;
+        static const AkUniqueID P2_ATTACK = 3016763366U;
+        static const AkUniqueID P2_CHARGE = 1669679186U;
+        static const AkUniqueID P2_DEFEND = 2258446150U;
+        static const AkUniqueID P2_MISS = 639554226U;
+        static const AkUniqueID P2_SPECIAL = 3741196759U;
+        static const AkUniqueID PLAYER_DEATH = 3083087645U;
+        static const AkUniqueID UI_CLICK = 2249769530U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID GAME = 702482391U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
