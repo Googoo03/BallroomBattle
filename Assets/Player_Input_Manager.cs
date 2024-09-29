906cefc75421d5d2824bfe69bd0c3fbf3cf71fdd
Googoo03 <nicholaskaralis6@gmail.com>
2024-09-29 10:41:37 -0700

    arrow flip and sprite blocking

--- Assets/Player_Input_Manager.cs
+++ Assets/Player_Input_Manager.cs
@@ -94,7 +94,7 @@ public class Player_Input_Manager : MonoBehaviour
 
                 arrow.setRotation(rot);
                 arrow.setColor(Color.red);
-                if(arrow.gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Arrow_Shake")) arrow.gameObject.GetComponent<Animator>().SetTrigger("Reset");
+                arrow.gameObject.GetComponent<Animator>().SetTrigger("Reset");
             }
         }
     }
